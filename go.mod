8f59ecc7af4473a9e6063426378dfca3aed922e8
PanGan21 <ganelis.pan@gmail.com>
2021-02-21 16:50:17 +0100

    server setup

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-redis/redis/v7 v7.4.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,40 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	fmt.Println("works")
-}
-
-// User type and json mapping
-type User struct {
-	ID       int64  `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-var user = User{
-	ID:       1,
-	Username: "username",
-	Password: "password",
-}
-
-// CreateToken creates and returns a token with expiration time 15 minutes
-func CreateToken(userID int64) (string, error) {
-	atClaims := jwt.MapClaims{}
-	atClaims["user_id"] = userID
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-
-	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err := at.SignedString([]byte(os.Getenv("ACESS_SECRET")))
-	if err != nil {
-		return "", err
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file detected.")
 	}
-	return token, nil
+}
+
+// NewRedisDB return a new redis client
+func NewRedisDB(host string, port string, password string) (*redis.Client) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: host + ":" + port,
+		Password: password,
+		DB: 0,
+	})
+	return redisClient
+}
+
+func main() {
+	appAddr := ":" + os.Getenv("PORT")
+
+	// setup redis
+	// redisHost := os.Getenv("REDIS_HOST")
+	// redisPort := os.Getenv("REDIS_PORT")
+	// redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	// redisClient := NewRedisDB(redisHost, redisPort, redisPassword)
+
+	var router = gin.Default()
+
+	srv := &http.Server{
+		Addr: appAddr,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Listen: %s\n", err)
+		}
+	}()
+	//	Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting.")
 }

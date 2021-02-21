28bc234633b13be6ab9f71dfde3123ecdd119b28
PanGan21 <ganelis.pan@gmail.com>
2021-02-21 17:59:09 +0100

    add auth service

--- auth/auth.go
+++ auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/go-redis/redis"
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+)
 
 // TokenDetails defines the details of a token
 type TokenDetails struct {
@@ -31,9 +37,66 @@ type service struct {
 }
 
 // Auth implements the AuthInterface
-// var _ AuthInterface = &service{}
+var _ AuthInterface = &service{}
 
 // NewAuth returns a new Authentication service with redis client injected
 func NewAuth(client *redis.Client) *service {
 	return &service{client: client}
+}
+
+// Saves token metadata to Redis
+func (tk *service) CreateAuth(userID string, td *TokenDetails) error {
+	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	atCreated, err := tk.client.Set(td.TokenUUID, userID, at.Sub(now)).Result()
+	if err != nil {
+		return err
+	}
+	rtCreated, err := tk.client.Set(td.RefreshUUID, userID, rt.Sub(now)).Result()
+	if err != nil {
+		return err
+	}
+	if atCreated == "0" || rtCreated == "0" {
+		return errors.New("no record inserted")
+	}
+	return nil
+}
+
+func (tk *service) FetchAuth(tokenUUID string) (string, error) {
+	userid, err := tk.client.Get(tokenUUID).Result()
+	if err != nil {
+		return "", err
+	}
+	return userid, nil
+}
+
+func (tk *service) DeleteRefresh(refreshUUID string) error {
+	//delete refresh token
+	deleted, err := tk.client.Del(refreshUUID).Result()
+	if err != nil || deleted == 0 {
+		return err
+	}
+	return nil
+}
+
+func (tk *service) DeleteTokens(authD *AccessDetails) error {
+	//get the refresh uuid
+	refreshUUID := fmt.Sprintf("%s++%s", authD.TokenUUID, authD.UserID)
+	//delete access token
+	deletedAt, err := tk.client.Del(authD.TokenUUID).Result()
+	if err != nil {
+		return err
+	}
+	//delete refresh token
+	deletedRt, err := tk.client.Del(refreshUUID).Result()
+	if err != nil {
+		return err
+	}
+	//When the record is deleted, the return value is 1
+	if deletedAt != 1 || deletedRt != 1 {
+		return errors.New("something went wrong")
+	}
+	return nil
 }
